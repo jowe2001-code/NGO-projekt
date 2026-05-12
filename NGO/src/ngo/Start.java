b74512a59c471aa1a95f039533c248b6e01b8bc0
Jonas Werner <jowe2001@hotmail.com>
2026-05-12 21:26:44 +0200

    inloggnings video

--- NGO/src/ngo/Start.java
+++ NGO/src/ngo/Start.java
@@ -2,15 +2,19 @@ package ngo;
 
 import oru.inf.InfDB;
 import oru.inf.InfException;
-
+        
 public class Start {
+    
+   private static InfDB idb; 
+    
     public static void main(String[] args) {
-        try {
-            InfDB db = new InfDB("SDGSweden", "3306", 
-                                 "dbAdmin2024", "dbAdmin2024PW");
-            System.out.println("Uppkoppling lyckades!");
-        } catch (InfException ex) {
-            System.out.println("Fel: " + ex.getMessage());
+        
+        try{
+            idb = new InfDB("SDGSweden", "3306", "dbAdmin2024", "dbAdmin2024PW");
+            new Inloggning(idb).setVisible(true);
+            
+        } catch(InfException ex){
+            System.out.println(ex.getMessage());    
         }
     }
 }
