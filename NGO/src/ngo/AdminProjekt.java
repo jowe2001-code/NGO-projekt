4ddc2bdfe34623ed1d0eb392ed51b465da919db2
Niklas Nygård <niklas.nygard@telia.com>
2026-05-29 12:47:49 +0200

    Knappar adminProjekt

--- NGO/src/ngo/AdminProjekt.java
+++ NGO/src/ngo/AdminProjekt.java
@@ -126,6 +126,10 @@ public class AdminProjekt extends javax.swing.JFrame {
 
         jScrollPane1 = new javax.swing.JScrollPane();
         tblAdminProjekt = new javax.swing.JTable();
+        btnVisaHandläggare = new javax.swing.JButton();
+        btnSparaÄndringar = new javax.swing.JButton();
+        btnTaBort = new javax.swing.JButton();
+        btnNyttProjekt = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -142,20 +146,46 @@ public class AdminProjekt extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(tblAdminProjekt);
 
+        btnVisaHandläggare.setText("Visa handläggare");
+
+        btnSparaÄndringar.setText("Spara");
+
+        btnTaBort.setText("Ta bort projekt");
+
+        btnNyttProjekt.setText("Nytt projekt");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1001, Short.MAX_VALUE)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 1001, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addComponent(btnVisaHandläggare)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(btnNyttProjekt)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnTaBort)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(btnSparaÄndringar)))
                 .addContainerGap())
         );
+
+        layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {btnNyttProjekt, btnTaBort});
+
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 499, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnVisaHandläggare)
+                    .addComponent(btnSparaÄndringar)
+                    .addComponent(btnTaBort)
+                    .addComponent(btnNyttProjekt))
                 .addContainerGap())
         );
 
@@ -188,6 +218,10 @@ public class AdminProjekt extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnNyttProjekt;
+    private javax.swing.JButton btnSparaÄndringar;
+    private javax.swing.JButton btnTaBort;
+    private javax.swing.JButton btnVisaHandläggare;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable tblAdminProjekt;
     // End of variables declaration//GEN-END:variables
