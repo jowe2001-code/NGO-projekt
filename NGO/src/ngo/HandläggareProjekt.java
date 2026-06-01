a6f522e511ab9837da4c28c2f33d770571eb6e8d
Niklas Nygård <niklas.nygard@telia.com>
2026-06-01 12:43:16 +0200

    fix

--- NGO/src/ngo/HandläggareProjekt.java
+++ NGO/src/ngo/HandläggareProjekt.java
@@ -76,7 +76,7 @@ public class HandläggareProjekt extends javax.swing.JFrame {
         btnLäggTill = new javax.swing.JButton();
         btnTaBort = new javax.swing.JButton();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
         tblHandläggare.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
